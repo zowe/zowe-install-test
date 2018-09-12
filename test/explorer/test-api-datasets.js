44923df483c4683dd3c376a166fc64d9f6f9ec4d
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2018-09-12 15:37:28 -0400

    improve test cases of explorer server
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

---
+++ test/explorer/test-api-datasets.js
@@ -0,0 +1,88 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2018
+ */
+
+const _ = require('lodash');
+const expect = require('chai').expect;
+const debug = require('debug')('test:explorer:api-datasets');
+const axios = require('axios');
+
+let REQ, username, password;
+let testDir;
+const DS_PATTERN_TO_TEST = 'TCPIP.T*';
+const DS_TO_TEST = 'TCPIP.TCPIP.DATA';
+
+// allow self signed certs
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+describe('test explorer server datasets api', function() {
+  before('verify environment variables', function() {
+    expect(process.env.SSH_HOST, 'SSH_HOST is not defined').to.not.be.empty;
+    expect(process.env.SSH_USER, 'SSH_USER is not defined').to.not.be.empty;
+    expect(process.env.SSH_PASSWD, 'SSH_PASSWD is not defined').to.not.be.empty;
+    expect(process.env.ZOWE_ROOT_DIR, 'ZOWE_ROOT_DIR is not defined').to.not.be.empty;
+    expect(process.env.ZOWE_EXPLORER_SERVER_HTTPS_PORT, 'ZOWE_EXPLORER_SERVER_HTTPS_PORT is not defined').to.not.be.empty;
+
+    REQ = axios.create({
+      baseURL: `https://${process.env.SSH_HOST}:${process.env.ZOWE_EXPLORER_SERVER_HTTPS_PORT}`,
+      timeout: 30000,
+    });
+    username = process.env.SSH_USER;
+    password = process.env.SSH_PASSWD;
+    debug(`Explorer server URL: https://${process.env.SSH_HOST}:${process.env.ZOWE_EXPLORER_SERVER_HTTPS_PORT}`);
+
+    testDir = process.env.ZOWE_ROOT_DIR;
+  });
+
+  it(`should be able to list data sets of ${DS_PATTERN_TO_TEST}`, function() {
+    const req = {
+      method: 'get',
+      url: '/Atlas/api/datasets/' + encodeURIComponent(DS_PATTERN_TO_TEST),
+      auth: {
+        username,
+        password,
+      }
+    };
+    debug('request', req);
+
+    return REQ.request(req)
+      .then(function(res) {
+        debug('response', _.pick(res, ['status', 'statusText', 'headers', 'data']));
+
+        expect(res).to.have.property('status');
+        expect(res.status).to.equal(200);
+        expect(res.data).to.be.an('array');
+        expect(res.data).to.include(DS_TO_TEST);
+      });
+  });
+
+  it(`should be able to get content of data set ${DS_TO_TEST}`, function() {
+    const req = {
+      method: 'get',
+      url: '/Atlas/api/datasets/' + encodeURIComponent(DS_TO_TEST) + '/content',
+      auth: {
+        username,
+        password,
+      }
+    };
+    debug('request', req);
+
+    return REQ.request(req)
+      .then(function(res) {
+        debug('response', _.pick(res, ['status', 'statusText', 'headers', 'data']));
+
+        expect(res).to.have.property('status');
+        expect(res.status).to.equal(200);
+        expect(res.data).to.be.an('object');
+        expect(res.data).to.have.property('records');
+        expect(res.data.records).to.be.a('string');
+        expect(res.data.records).to.include('TCPIP.DATA');
+      });
+  });
+});
---
+++ test/explorer/test-api-jobs.js
@@ -0,0 +1,65 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2018
+ */
+
+const _ = require('lodash');
+const expect = require('chai').expect;
+const debug = require('debug')('test:explorer:api-jobs');
+const axios = require('axios');
+
+const { ZOWE_JOB_NAME } = require('../constants');
+
+let REQ, username, password;
+
+// allow self signed certs
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+describe('test explorer server jobs api', function() {
+  before('verify environment variables', function() {
+    expect(process.env.SSH_HOST, 'SSH_HOST is not defined').to.not.be.empty;
+    expect(process.env.SSH_USER, 'SSH_USER is not defined').to.not.be.empty;
+    expect(process.env.SSH_PASSWD, 'SSH_PASSWD is not defined').to.not.be.empty;
+    expect(process.env.ZOWE_EXPLORER_SERVER_HTTPS_PORT, 'ZOWE_EXPLORER_SERVER_HTTPS_PORT is not defined').to.not.be.empty;
+
+    REQ = axios.create({
+      baseURL: `https://${process.env.SSH_HOST}:${process.env.ZOWE_EXPLORER_SERVER_HTTPS_PORT}`,
+      timeout: 30000,
+    });
+    username = process.env.SSH_USER;
+    password = process.env.SSH_PASSWD;
+    debug(`Explorer server URL: https://${process.env.SSH_HOST}:${process.env.ZOWE_EXPLORER_SERVER_HTTPS_PORT}`);
+  });
+
+  it(`should be able to list jobs and have a job ${ZOWE_JOB_NAME}`, function() {
+    const req = {
+      method: 'get',
+      url: '/Atlas/api/jobs',
+      params: {
+        prefix: 'ZOWE*',
+        owner: '*'
+      },
+      auth: {
+        username,
+        password,
+      }
+    };
+    debug('request', req);
+
+    return REQ.request(req)
+      .then(function(res) {
+        debug('response', _.pick(res, ['status', 'statusText', 'headers', 'data']));
+        expect(res).to.have.property('status');
+        expect(res.status).to.equal(200);
+        expect(res.data).to.be.an('array');
+        expect(res.data).to.have.lengthOf(1);
+        expect(res.data[0]).to.have.all.keys('name', 'jobInstances');
+        expect(res.data[0].name).to.equal(ZOWE_JOB_NAME);
+      });
+  });
+});
