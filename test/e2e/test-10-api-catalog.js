99a30c524f371df5cfef1ed7747cfcde42294d97
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2018-10-29 16:34:28 -0400

    add api catalog login check
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- test/e2e/test-10-api-catalog.js
+++ test/e2e/test-10-api-catalog.js
@@ -15,10 +15,13 @@ const addContext = require('mochawesome/addContext');
 const testName = path.basename(__filename, path.extname(__filename));
 
 const {
+  DEFAULT_PAGE_LOADING_TIMEOUT,
   MVD_IFRAME_APP_CONTENT,
   saveScreenshot,
   getDefaultDriver,
   waitUntilElement,
+  getElement,
+  getElements,
   loginMVD,
   launchApp,
   locateApp,
@@ -36,11 +39,11 @@ describe(`test ${APP_TO_TEST}`, function() {
     expect(process.env.SSH_USER, 'SSH_USER is not defined').to.not.be.empty;
     expect(process.env.SSH_PASSWD, 'SSH_PASSWD is not defined').to.not.be.empty;
     expect(process.env.ZOWE_ZLUX_HTTPS_PORT, 'ZOWE_ZLUX_HTTPS_PORT is not defined').to.not.be.empty;
-  
+
     // init webdriver
     driver = await getDefaultDriver();
     debug('webdriver initialized');
-  
+
     // load MVD login page
     await loginMVD(
       driver,
@@ -49,44 +52,82 @@ describe(`test ${APP_TO_TEST}`, function() {
       process.env.SSH_PASSWD
     );
   });
-  
+
   it('should launch app correctly', async function() {
     // load app
     await launchApp(driver, APP_TO_TEST);
     const app = await locateApp(driver, APP_TO_TEST);
     expect(app).to.be.an('object');
     debug('app launched');
-    
+
     // save screenshot
     const file = await saveScreenshot(driver, testName, 'app-loading');
     addContext(this, file);
-    
+
     // locate app iframe
     const iframe = await waitUntilIframe(driver, 'rs-com-mvd-iframe-component > iframe', app);
     expect(iframe).to.be.an('object');
     debug('app iframe found');
-    
+
     // wait for atlas iframe
     const atlas = await waitUntilIframe(driver, 'iframe#zluxIframe');
     expect(atlas).to.be.an('object');
     debug('iframe is ready');
-    
+
     // to avoid StaleElementReferenceError, find the iframes context again
     await switchToIframeAppContext(driver, APP_TO_TEST, MVD_IFRAME_APP_CONTENT);
-        
-    // // wait for page is loaded
-    const searchBox = await waitUntilElement(driver, '.search-bar');
-    expect(searchBox).to.be.an('object');
-    
+
+    // wait for login form to be loaded
+    const loginForm = await waitUntilElement(driver, '#login-form');
+    expect(loginForm).to.be.an('object');
+    debug('login form is ready');
+
+    // save screenshot
+    await saveScreenshotWithIframeAppContext(this, driver, testName, 'app-loaded', APP_TO_TEST, MVD_IFRAME_APP_CONTENT);
+
+    // the form should have already been pre-filled
+    const username = await getElement(driver, '#login-form input#username');
+    expect(username).to.be.an('object');
+    const usernameVal = await username.getAttribute('value');
+    expect(usernameVal).to.equal('user'); // default user
+
+    const password = await getElement(driver, '#login-form input#password');
+    expect(password).to.be.an('object');
+    const passwordVal = await password.getAttribute('value');
+    expect(passwordVal).to.equal('user'); // default password
+
+    const loginButton = await getElement(driver, '#login-form button[type=submit]');
+    expect(loginButton).to.be.an('object');
+    await driver.sleep(10 * 1000);
+    loginButton.click();
+
+    // wait for page is loaded
+    try {
+      const searchBox = await waitUntilElement(driver, '.search-bar');
+      expect(searchBox).to.be.an('object');
+    } catch (e) {
+      // try to save screenshot for debug purpose
+      await saveScreenshotWithIframeAppContext(this, driver, testName, 'login-failed', APP_TO_TEST, MVD_IFRAME_APP_CONTENT);
+
+      const errName = e && e.name;
+      if (errName === 'TimeoutError') {
+        expect(errName).to.not.equal('TimeoutError');
+      } else {
+        expect(e).to.be.null;
+      }
+    }
+
     const productNameText = await getElementText(driver, '.product-name');
     expect(productNameText).to.equal(APP_TO_TEST);
 
+    // TODO: check listed "Available APIs"
+
     debug('page is fully loaded');
-    
+
     // save screenshot
-    await saveScreenshotWithIframeAppContext(this, driver, testName, 'app-loaded', APP_TO_TEST, MVD_IFRAME_APP_CONTENT);
+    await saveScreenshotWithIframeAppContext(this, driver, testName, 'login-successfully', APP_TO_TEST, MVD_IFRAME_APP_CONTENT);
   });
-    
+
   after('quit webdriver', async function() {
     // quit webdriver
     if (driver) {
