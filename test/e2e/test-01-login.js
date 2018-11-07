07bbd5547deccaef828807675a6dcdb989b42cd1
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2018-11-07 16:50:50 -0500

    should ignore case to compare
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- test/e2e/test-01-login.js
+++ test/e2e/test-01-login.js
@@ -158,7 +158,7 @@ describe('test MVD login page', function() {
     let error = await getElementText(driver, 'p.login-error');
     expect(error).to.be.a('string');
     error = error.trim();
-    expect(error).to.match(/authentication\s*failed/g);
+    expect(error).to.match(/authentication\s*failed/i);
   });
 
 
