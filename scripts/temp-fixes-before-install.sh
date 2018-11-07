6e4c46f5313bcab70762a11bb8f800c6c254c264
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2018-11-07 15:33:51 -0500

    write env to .profile
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- scripts/temp-fixes-before-install.sh
+++ scripts/temp-fixes-before-install.sh
@@ -31,7 +31,11 @@ CI_PWD=$(pwd)
 # NODE_HOME is not specified on the pre-reqs image
 if [ -z "$NODE_HOME" ]; then
   echo "[${SCRIPT_NAME}] NODE_HOME is missing, need to fix:"
-  export NODE_HOME=/Z23B/usr/lpp/IBM/cnj/IBM/node-v6.13.0-os390-s390x
+  # write to ~/.profile
+  if [ ! -f "~/.profile" ]; then
+    touch ~/.profile
+  fi
+  echo "export NODE_HOME=/Z23B/usr/lpp/IBM/cnj/IBM/node-v6.13.0-os390-s390x" >> ~/.profile
 else
   echo "[${SCRIPT_NAME}] NODE_HOME is in place, no need to fix."
 fi
