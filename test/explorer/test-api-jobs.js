0383a9714f7807a936821e6877b5c64223c3afe6
stevenhorsman <steven@uk.ibm.com>
2019-03-04 10:54:38 +0000

    Attempt to update api responses
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- test/explorer/test-api-jobs.js
+++ test/explorer/test-api-jobs.js
@@ -65,10 +65,10 @@ describe('test explorer server jobs api', function() {
 
         expect(res).to.have.property('status');
         expect(res.status).to.equal(200);
-        expect(res.data).to.be.an('array');
-        expect(res.data).to.have.lengthOf(1);
-        expect(res.data[0]).to.have.any.keys('jobName', 'jobId', 'owner', 'status', 'type', 'subsystem');
-        expect(res.data[0].jobName).to.equal(ZOWE_JOB_NAME);
+        expect(res.data.items).to.be.an('array');
+        expect(res.data.items).to.have.lengthOf(1);
+        expect(res.data.items[0]).to.have.any.keys('jobName', 'jobId', 'owner', 'status', 'type', 'subsystem');
+        expect(res.data.items[0].jobName).to.equal(ZOWE_JOB_NAME);
       });
   });
 });
