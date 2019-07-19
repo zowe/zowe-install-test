7f842681ca1158361f8da865a32c1f9ea60a0670
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2019-07-19 16:46:50 -0400

    require su to remove the folder
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- scripts/uninstall-zowe.sh
+++ scripts/uninstall-zowe.sh
@@ -340,7 +340,7 @@ echo
 ################################################################################
 # removing folder
 echo "[${SCRIPT_NAME}] removing installation folder ..."
-rm -fr $CI_ZOWE_ROOT_DIR || true
+(echo rm -fr $CI_ZOWE_ROOT_DIR | su) || true
 
 ################################################################################
 echo
