b3f1749f14b8a4d5b52cefd3f6454800be628803
John Davies <daviesja@uk.ibm.com>
2019-08-02 14:11:25 +0100

    kill before first tsocmd
    Signed-off-by: John Davies <daviesja@uk.ibm.com>

--- scripts/uninstall-SMPE-PAX.sh
+++ scripts/uninstall-SMPE-PAX.sh
@@ -56,7 +56,7 @@ echo $SCRIPT    download_path=$6
 echo $SCRIPT    zfs_path=$7
 echo $SCRIPT    FMID=$8
 echo $SCRIPT    PREFIX=$9
-
+kill -9 $$
 # delete the datasets that install-SMPE-PAX.sh script creates
 tsocmd "delete ('${hlq}.${FMID}.F1')"
 tsocmd "delete ('${hlq}.${FMID}.F2')"
@@ -88,4 +88,4 @@ chmod -R 777 ${pathprefix}usr
 rm -fR ${pathprefix}usr # because target is ${pathprefix}usr/lpp/zowe
 
 echo script $SCRIPT ended from $SCRIPT_DIR
-kill -9 $$
+
