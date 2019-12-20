494a6296dd047c11ba91e41359e542badd8ee3c2
stevenhorsman <steven@uk.ibm.com>
2019-12-20 17:58:06 +0000

    Add quotes
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- scripts/install-zowe.sh
+++ scripts/install-zowe.sh
@@ -557,7 +557,7 @@ else
   echo "calling zowe-install-proc.sh with"
   echo "    ZOWE_DSN_PREFIX=$USER.ZWE"
   echo "    ZOWE_SERVER_PROCLIB_DSNAME=$CIZT_PROCLIB_DS"
-  RUN_SCRIPT=./zowe-install-proc.sh $USER.ZWE $CIZT_PROCLIB_DS
+  RUN_SCRIPT="./zowe-install-proc.sh $USER.ZWE $CIZT_PROCLIB_DS"
   run_script_with_timeout $RUN_SCRIPT 3600
   EXIT_CODE=$?
   if [[ "$EXIT_CODE" != "0" ]]; then
