593df451b8bba65481af8dacbd8a137f28a70704
John Davies <daviesja@uk.ibm.com>
2020-01-14 16:20:44 +0000

    call script to run ZWESECUR
    Signed-off-by: John Davies <daviesja@uk.ibm.com>

---
+++ scripts/create-security-defn.sh
@@ -0,0 +1,157 @@
+#!/bin/sh
+
+# Function: Run ZWESECUR job to create security definitions
+
+# Inputs
+# files/ZWESECUR.jcl
+# $CIZT_INSTALL_DIR must be set
+
+# identify this script
+# SCRIPT_DIR="$(dirname $0)"
+SCRIPT_DIR=`pwd`
+SCRIPT="$(basename $0)"
+echo script $SCRIPT started from $SCRIPT_DIR
+
+# allow us to customize /tmp folder
+if [ -z "${CIZT_TMP}" ]; then
+  CIZT_TMP=/tmp
+fi
+
+operdir=$SCRIPT_DIR         # this is where opercmd should be available
+
+head -1 $operdir/opercmd | grep REXX 1> /dev/null 2> /dev/null
+if [[ $? -ne 0 ]]
+then
+    echo $SCRIPT ERROR: opercmd not found in $operdir or is not valid REXX 
+    echo $SCRIPT INFO: CWD is `pwd`
+    exit 9
+fi
+
+################################################################################
+# Wrap call into $()
+#
+# NOTE: This function exists to solve the issue calling tsocmd/submit/cp directly
+#       in pipeline will not exit properly.
+################################################################################
+function wrap_call {
+  echo "[wrap_call] $@ >>>"
+  CALL_RESULT=$($@)
+  printf "%s\n[wrap_call] <<<\n" "$CALL_RESULT"
+}
+
+function runJob {
+
+    echo; echo $SCRIPT function runJob started
+    jclname=$1
+
+    echo $SCRIPT jclname=$jclname #jobname=$jobname
+    ls -l $jclname
+
+    # show JCL for debugging purpose
+    echo $SCRIPT ====================== content start ======================
+    cat $jclname
+    echo $SCRIPT ====================== content end ========================
+
+    # submit the job using the USS submit command
+    submit $jclname > $CIZT_TMP/submit.job.$$.out
+    if [[ $? -ne 0 ]]
+    then
+        echo $SCRIPT ERROR: submit JCL $jclname failed
+        return 1
+    else
+        echo $SCRIPT INFO: JCL $jclname submitted
+    fi
+
+    # capture JOBID of submitted job
+    jobid=`cat $CIZT_TMP/submit.job.$$.out \
+        | sed "s/.*JOB JOB\([0-9]*\) submitted.*/\1/"`
+    rm $CIZT_TMP/submit.job.$$.out 2> /dev/null 
+
+    # echo; echo $SCRIPT JOBID=$jobid
+
+    # wait for job to finish
+    jobdone=0
+    for secs in 1 1 1 5 5 5
+    do
+        sleep $secs
+        $operdir/opercmd "\$DJ${jobid},CC" > $CIZT_TMP/dj.$$.cc
+            # $DJ gives ...
+            # ... $HASP890 JOB(JOB1)      CC=(COMPLETED,RC=0)  <-- accept this value
+            # ... $HASP890 JOB(GIMUNZIP)  CC=()  <-- reject this value
+        
+        grep "$HASP890 JOB(.*) *CC=(.*)" $CIZT_TMP/dj.$$.cc > /dev/null
+        if [[ $? -eq 0 ]]
+        then
+            jobname=`sed -n "s/.*$HASP890 JOB(\(.*\)) *CC=(.*).*/\1/p" $CIZT_TMP/dj.$$.cc`
+            if [[ ! -n "$jobname" ]]
+            then
+                jobname=empty
+            fi 
+        else
+            jobname=unknown
+        fi
+        echo $SCRIPT INFO: Checking for completion of jobname $jobname jobid $jobid
+        
+        grep "CC=(..*)" $CIZT_TMP/dj.$$.cc > /dev/null   # ensure CC() is not empty
+        if [[ $? -eq 0 ]]
+        then
+            jobdone=1
+            break
+        fi
+    done
+    if [[ $jobdone -eq 0 ]]
+    then
+        echo $SCRIPT ERROR: job ${jobid} PID=$$ not run in time
+        echo $SCRIPT DISPLAY JOB output was:
+        cat $CIZT_TMP/dj.$$.cc
+        rm $CIZT_TMP/dj.$$.cc 2> /dev/null 
+        return 2
+    else
+        : # echo; echo $SCRIPT job JOB$jobid completed
+    fi
+
+    grep RC= $CIZT_TMP/dj.$$.cc > /dev/null
+    if [[ $? -ne 0 ]]
+    then
+        echo $SCRIPT ERROR: no return code for jobid $jobid PID=$$
+        echo $SCRIPT DISPLAY JOB output was:
+        cat $CIZT_TMP/dj.$$.cc
+        rm $CIZT_TMP/dj.$$.cc 2> /dev/null 
+        return 3
+    fi
+    
+    rc=`sed -n 's/.*RC=\([0-9]*\))/\1/p' $CIZT_TMP/dj.$$.cc`
+    # echo; echo $SCRIPT return code for JOB$jobid is $rc
+    rm $CIZT_TMP/dj.$$.cc 2> /dev/null 
+    if [[ $rc -gt 4 ]]
+    then
+        echo $SCRIPT ERROR: job "$jobname(JOB$jobid)" failed, RC=$rc 
+        return 4
+    fi
+    # echo; echo $SCRIPT function runJob ended
+}
+
+
+
+# sed \
+#     -e "s+@zfs_path@+${zfs_path}+" \
+#     -e "s+&FMID\.+${FMID}+" \
+#     -e "s+@PREFIX@+${PREFIX}+" \
+#     -e "/<GIMUNZIP>/ a\\
+#     <TEMPDS volume=\"$CUSTOMIZED_VOLSER\"></TEMPDS>"\
+#     -e "/archid=/ a\\
+#     \ \ \ \ \ \ \ \ \ volume=\"$CUSTOMIZED_VOLSER\""\
+#     $zfs_path/gimunzip.jcl0 > $zfs_path/gimunzip.jcl1
+
+
+
+# Run the GIMUNZIP job
+runJob $CIZT_INSTALL_DIR/../files/ZWESECUR.jcl
+if [[ $? -ne 0 ]]
+then
+    echo $SCRIPT ERROR: ZWESECUR JOB failed
+    exit 1
+fi
+
+
+echo script $SCRIPT ended from $SCRIPT_DIR
--- scripts/install-zowe.sh
+++ scripts/install-zowe.sh
@@ -310,6 +310,19 @@ if [[ "$CI_UNINSTALL" = "yes" ]]; then
   fi
 fi
 
+# Run security job to create the SAF definitions for Zowe
+cd $CIZT_INSTALL_DIR
+# 
+echo "[${SCRIPT_NAME}] create the SAF definitions for Zowe ..."
+RUN_SCRIPT="./create-security-defn.sh "
+run_script_with_timeout "${RUN_SCRIPT}" 1800
+
+if [ $? -ne 0 ]; then
+  echo "[${SCRIPT_NAME}][error] create the SAF definitions failed"
+  exit 1
+fi
+echo
+
 rm -fr ${CIZT_INSTALL_DIR}/extracted && mkdir -p ${CIZT_INSTALL_DIR}/extracted
 if [[ "$CI_IS_SMPE" = "yes" ]]; then
   cd $CIZT_INSTALL_DIR
