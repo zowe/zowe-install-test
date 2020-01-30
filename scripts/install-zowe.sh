cbc0131f54aedb13a8b0811c25e91f1f499cd525
John Davies <daviesja@uk.ibm.com>
2020-01-30 14:10:07 +0000

    make zowe-xmem-apf.sh executable
    Signed-off-by: John Davies <daviesja@uk.ibm.com>

--- scripts/install-zowe.sh
+++ scripts/install-zowe.sh
@@ -517,6 +517,8 @@ echo "  zss loadlib   ${CIZT_ZSS_LOADLIB_DS_NAME}"
 
 RUN_SCRIPT=./zowe-xmem-apf.sh
 cd $CIZT_INSTALL_DIR
+ls -l     $RUN_SCRIPT
+chmod a+x $RUN_SCRIPT
 echo "[${SCRIPT_NAME}] calling $RUN_SCRIPT from directory $(pwd)"
 run_script_with_timeout "$RUN_SCRIPT \
   ${CIZT_INSTALL_DIR}/opercmd \
