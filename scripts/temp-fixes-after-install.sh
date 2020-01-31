9b9aee54e02a0a1593a3a6bf496b2e01af685939
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2020-01-31 11:24:50 -0500

    grant execute permission
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- scripts/temp-fixes-after-install.sh
+++ scripts/temp-fixes-after-install.sh
@@ -94,6 +94,8 @@ if [ -f run-zowe.sh ]; then
     __IPC_CLEANUP=1 ${CUSTOM_NODE_HOME}/bin/node --version"\
     run-zowe.sh > ${CIZT_TMP}/run-zowe.sh.tmp
   echo mv ${CIZT_TMP}/run-zowe.sh.tmp run-zowe.sh | su
+  # give execute permission
+  echo chmod 750 run-zowe.sh | su
 fi
 echo
 
