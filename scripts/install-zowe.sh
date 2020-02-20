7bfe7f83e3a8acf23384ebff1e7e3665a2a4af0d
John Davies <daviesja@uk.ibm.com>
2020-02-20 09:57:19 +0000

    check JCL encoding
    Signed-off-by: John Davies <daviesja@uk.ibm.com>

--- scripts/install-zowe.sh
+++ scripts/install-zowe.sh
@@ -309,9 +309,14 @@ for job in \
     Z7REJECT \
     Z8DEALOC 
 do    
+  echo     ${job}.jcl before
+  od -xc   ${job}.jcl | head -2
   if [ -f "${job}.jcl" ]; then
     ensure_script_encoding ${job}.jcl "//" # tell script to check jcl files for "//" instead of default for shell scripts
   fi
+  echo     ${job}.jcl after
+  od -xc   ${job}.jcl | head -2
+  exit 3
 done
 
 if [ -f "opercmd" ]; then
