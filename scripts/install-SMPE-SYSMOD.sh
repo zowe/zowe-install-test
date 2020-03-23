d0851a47a8b60f5e780c8b228230d42c4cef98b6
stevenhorsman <steven@uk.ibm.com>
2020-03-23 16:03:07 +0000

    Try wrap_call
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- scripts/install-SMPE-SYSMOD.sh
+++ scripts/install-SMPE-SYSMOD.sh
@@ -115,11 +115,12 @@ function wrap_call {
 }
 
 # Clean up sysmod data sets
-wrap_call tsocmd DELETE "'${hlq}.ZOWE.${FMID}.$SYSMOD1'"
-wrap_call tsocmd DELETE "'${hlq}.ZOWE.${FMID}.$SYSMOD2'"
+wrap_call "tsocmd DELETE \"'${hlq}.ZOWE.${FMID}.$SYSMOD1'\"" 180
+wrap_call "tsocmd DELETE \"'${hlq}.ZOWE.${FMID}.$SYSMOD2'\"" 180
 
-echo "Before sysmod1 allocate: $(tsocmd listds "'${hlq}.ZOWE.${FMID}.$SYSMOD1'" history status members)"
-echo "Before sysmod2 allocate: $(tsocmd listds "'${hlq}.ZOWE.${FMID}.$SYSMOD2'" history status members)"
+echo "Before sysmod1 allocate:"
+wrap_call "tsocmd listds \"'${hlq}.ZOWE.${FMID}.$SYSMOD1'\" history status members" 180
+wrap_call "tsocmd listds \"'${hlq}.ZOWE.${FMID}.$SYSMOD2'\" history status members" 180
 
 csidsn=$csihlq.CSI
 
@@ -347,14 +348,16 @@ do
     if [[ $smpejob = Z1ALLOC ]]
     then
         echo script $SCRIPT copying USS sysmod files to datasets
-        echo "Before sysmod1 copy: $(tsocmd listds "'${hlq}.ZOWE.${FMID}.$SYSMOD1'" history status members)"
-        echo "Before sysmod2 copy: $(tsocmd listds "'${hlq}.ZOWE.${FMID}.$SYSMOD2'" history status members)"
+        echo "Before sysmod1 copies:"
+        wrap_call tsocmd listds "'${hlq}.ZOWE.${FMID}.$SYSMOD1'" history status members
+        wrap_call tsocmd listds "'${hlq}.ZOWE.${FMID}.$SYSMOD2'" history status members
         echo "uss sysmod 1: $(ls -al $download_path/ZOWE.$FMID.$SYSMOD1)"
         echo "uss sysmod 2: $(ls -al $download_path/ZOWE.$FMID.$SYSMOD2)"
-        cp $download_path/ZOWE.$FMID.$SYSMOD1 "//'${hlq}.ZOWE.${FMID}.$SYSMOD1'"
-        cp $download_path/ZOWE.$FMID.$SYSMOD2 "//'${hlq}.ZOWE.${FMID}.$SYSMOD2'"
-        echo "After sysmod1 copy: $(tsocmd listds "'${hlq}.ZOWE.${FMID}.$SYSMOD1'" history status members)"
-        echo "After sysmod2 copy: $(tsocmd listds "'${hlq}.ZOWE.${FMID}.$SYSMOD2'" history status members)"
+        wrap_call cp $download_path/ZOWE.$FMID.$SYSMOD1 "//'${hlq}.ZOWE.${FMID}.$SYSMOD1'"
+        wrap_call cp $download_path/ZOWE.$FMID.$SYSMOD2 "//'${hlq}.ZOWE.${FMID}.$SYSMOD2'"
+        echo "After sysmod copy:"
+        wrap_call tsocmd listds "'${hlq}.ZOWE.${FMID}.$SYSMOD1'" history status members
+        wrap_call tsocmd listds "'${hlq}.ZOWE.${FMID}.$SYSMOD2'" history status members
         echo script $SCRIPT copy complete
     fi
 
