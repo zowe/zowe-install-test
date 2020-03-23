a00071235bcb580532fa7a9f16c47bf1f039f079
stevenhorsman <steven@uk.ibm.com>
2020-03-23 15:51:49 +0000

    Move function
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- scripts/install-SMPE-SYSMOD.sh
+++ scripts/install-SMPE-SYSMOD.sh
@@ -102,8 +102,19 @@ echo $SCRIPT    SYSMOD2=$SYSMOD2
 echo $SCRIPT    volser=$volser
 echo $SCRIPT    install=$install
 
-# Clean up sysmod data sets
+################################################################################
+# Wrap call into $()
+#
+# NOTE: This function exists to solve the issue calling tsocmd/submit/cp directly
+#       in pipeline will not exit properly.
+################################################################################
+function wrap_call {
+  echo "[wrap_call] $@ >>>"
+  CALL_RESULT=$($@)
+  printf "%s\n[wrap_call] <<<\n" "$CALL_RESULT"
+}
 
+# Clean up sysmod data sets
 wrap_call tsocmd DELETE "'${hlq}.ZOWE.${FMID}.$SYSMOD1'"
 wrap_call tsocmd DELETE "'${hlq}.ZOWE.${FMID}.$SYSMOD2'"
 
@@ -133,18 +144,6 @@ then
     echo $SCRIPT INFO: CWD is `pwd`
     exit 9
 fi
-
-################################################################################
-# Wrap call into $()
-#
-# NOTE: This function exists to solve the issue calling tsocmd/submit/cp directly
-#       in pipeline will not exit properly.
-################################################################################
-function wrap_call {
-  echo "[wrap_call] $@ >>>"
-  CALL_RESULT=$($@)
-  printf "%s\n[wrap_call] <<<\n" "$CALL_RESULT"
-}
 
 function runJob {
 
